9852ddddc140e41c0508c928d5f6ae30bf0e1509
unknown <john.meng014@gmail.com>
2022-01-18 17:09:34 -0500

    removed dbConfig from ignorefile

---
+++ config/dbConfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+    HOST: process.env.HOST, // your endpoint
+    USER: process.env.USER, // your username
+    PASSWORD: process.env.PASSWORD, // your password
+    DB: process.env.DB, // your db name
+    dialect: 'postgres',
+ }
+
+
+
+// module.exports = {
+//     HOST: 'user-stocks.ccq70wy37xi3.us-east-1.rds.amazonaws.com', // your endpoint
+//     USER: 'user_stock', // your username
+//     PASSWORD: '1q2w3e4r', // your password
+//     DB: 'user_stocks_db', // your db name
+//     dialect: 'postgres',
+//     }
