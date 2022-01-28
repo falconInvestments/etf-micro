f0f6b5e37c99cd09a6ef87418e3b91c15d07926b
unknown <john.meng014@gmail.com>
2022-01-28 11:42:23 -0500

    force false and istalled cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routers = require('./routes/userRouter.js');
 const eftrouter = require('./routes/etfRouter.js');
+const cors = require('cors')
 
 
 const app = express()
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use('/user', routers);
