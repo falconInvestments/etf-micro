a925b98d72ee27c3b41c55b5ecdae74e7c2eec39
unknown <john.meng014@gmail.com>
2022-01-31 16:15:50 -0500

    portforlio

--- models/etfModel.js
+++ models/etfModel.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         price: {
-            type: DataTypes.STRING,
+            type: DataTypes.DECIMAL(10,2),
             allowNull:false
         }, 
         portfolio: {
